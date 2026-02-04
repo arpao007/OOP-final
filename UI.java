58b92efc9532f6f87c1d5b303cfddcbeaa74dbf3
Babymomayy💓🪽 <maynp.08@gmail.com>
2026-02-04 23:34:37 +0700

    Improve comments in UI.java for better understanding
    Updated comments for clarity in UI.java.

--- UI.java
+++ UI.java
@@ -28,10 +28,10 @@ public class UI {
         mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
         frame.add(mainPanel);
         
-        textField = new JTextField();
-        textField.setFont(new Font("Arial", Font.BOLD, 28));
+        textField = new JTextField(); // Display
+        textField.setFont(new Font("Arial", Font.BOLD, 28)); // ตั้งค่าหน้าจอ
         textField.setHorizontalAlignment(JTextField.RIGHT);
-        textField.setEditable(false);
+        textField.setEditable(false); //กดได้แค่ปุ่ม ไม่สามารถพิมพ์จากแป้นพิมพ์ได้
         textField.setPreferredSize(new Dimension(0, 70));
         mainPanel.add(textField, BorderLayout.NORTH);
         
